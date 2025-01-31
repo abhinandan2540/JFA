30ced5a320f87f2d1552fd7af200c9df0b3afa79
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-01-31 21:48:33 +0530

    NLM

--- Chapter_02/NLM.java
+++ Chapter_02/NLM.java
@@ -1,5 +1,18 @@
 package Chapter_02;
 
-public class NLM {
+import java.util.Scanner;
 
+public class NLM {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter v: ");
+        int v=input.nextInt();
+        System.out.println("enter u: ");
+        int u=input.nextInt();
+        System.out.println("enter s: ");
+        int s=input.nextInt();
+
+        float ans=(v*v)-(u*u)/(2*9.8f*s);
+        System.out.println(ans);
+    }
 }
