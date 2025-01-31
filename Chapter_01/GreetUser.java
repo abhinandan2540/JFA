5c68bb2909fc8b55c16609fe25e09ad786a2c6d4
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-01-31 20:37:46 +0530

    hello user

--- Chapter_01/GreetUser.java
+++ Chapter_01/GreetUser.java
@@ -1,4 +1,14 @@
 package Chapter_01;
 
+import java.util.Scanner;
+
 public class GreetUser {
+    public static void main(String[] args) {
+
+         Scanner input=new Scanner(System.in);
+        System.out.println("enter name :");
+        System.out.println("Hello "+input.nextLine());
+
+
+    }
 }
