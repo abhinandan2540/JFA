cab87dc331af440c3a8ab7b447093710e1829161
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-01-31 19:08:12 +0530

    basic datatype operation

---
+++ Chapter_01/DataTypes.java
@@ -0,0 +1,11 @@
+package Chapter_01;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int a=12;
+        int b=23;
+        int c=56;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
