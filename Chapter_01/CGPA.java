ee9d6ae51b9f818caa80f5700e2ae15e365141a1
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-01-31 19:08:28 +0530

    cgpa calc

--- Chapter_01/CGPA.java
+++ Chapter_01/CGPA.java
@@ -1,4 +1,15 @@
 package Chapter_01;
 
 public class CGPA {
+    public static void main(String[] args) {
+//        finding out CGPA from 3 subjects
+        int sub1=85;
+        int sub2=98;
+        int sub3=99;
+        int totalNumber=sub1+sub2+sub3;
+//        total no of subject 3
+//        we define float values by .f integrating
+        float CGPA=(totalNumber/3.0f)/10;
+        System.out.println(CGPA);
+    }
 }
