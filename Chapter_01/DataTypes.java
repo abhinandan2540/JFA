f0bd7f63a98009d9f7b2c76652875d54bd7e6d73
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-01-31 20:23:43 +0530

    java datatypes

--- Chapter_01/DataTypes.java
+++ Chapter_01/DataTypes.java
@@ -2,10 +2,13 @@ package Chapter_01;
 
 public class DataTypes {
     public static void main(String[] args) {
-        int a=12;
-        int b=23;
-        int c=56;
-        int sum=a+b+c;
-        System.out.println(sum);
+//        datatypes in java
+//       int automatically includes byte,short, int
+        int num=1234;
+        long num2=456668778996665554L;
+        float num3=12.34f;
+        double num4=123.5; // default is double, if want then can place d/D
+        boolean num5=true;
+
     }
 }
---
+++ Chapter_01/GreetUser.java
@@ -0,0 +1,4 @@
+package Chapter_01;
+
+public class GreetUser {
+}
