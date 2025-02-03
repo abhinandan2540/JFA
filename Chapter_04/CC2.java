3d62a661a043493ec457152426c9256168210405
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-03 22:49:14 +0530

    add:logical_operator

---
+++ Chapter_04/CC2.java
@@ -0,0 +1,24 @@
+package Chapter_04;
+
+public class CC2 {
+    public static void main(String[] args) {
+
+      int age=21;
+      int minAge=20;
+
+      if (age>21 && minAge==20){
+          System.out.println("accepted");
+      }else{
+          System.out.println("not accepted");
+
+      }
+
+        int a=10;
+        if (a!=12){
+            System.out.println("it should be 10");
+        }else{
+            System.out.println("it is 10");
+        }
+
+    }
+}
