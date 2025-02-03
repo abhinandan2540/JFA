bb8f57f3ff6fe92a64e73d69832fab4fedda6698
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-03 23:15:44 +0530

    concepts

--- Chapter_04/ConditionalsConcept1.java
+++ Chapter_04/ConditionalsConcept1.java
@@ -1,4 +1,22 @@
 package Chapter_04;
 
 public class ConditionalsConcept1 {
+    public static void main(String[] args) {
+
+        int age=20;
+        if(age>18){
+            System.out.println("you can drive");
+        }else{
+            System.out.println("you cannot drive");
+        }
+
+        int age2=20;
+        if (age2==20){
+            System.out.println("allowed in bar boy");
+        }else{
+            System.out.println("get lost boy");
+        }
+
+
+    }
 }
