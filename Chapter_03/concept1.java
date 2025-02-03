921e71421c08e36cdce4ecaa16ace3d274f94cd8
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-03 19:30:57 +0530

    string concept 1

---
+++ Chapter_03/concept1.java
@@ -0,0 +1,23 @@
+package Chapter_03;
+
+public class concept1 {
+    public static void main(String[] args) {
+//        String name="alexa";
+//        System.out.println("Hi "+ name);
+
+//        int a=6;
+//        float b=5.6894f;
+//        System.out.printf("the vaue of a is %d and b is %f ",a,b);
+
+        int money=1200;
+        float rem=45.23f;
+        char f='F';
+        String guy="gemini";
+        System.out.printf("%s is saying he has remaining only %.2f out of %d, this is big %c ",guy,rem,money,f);
+
+
+
+
+
+    }
+}
