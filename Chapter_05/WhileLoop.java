c19afaa2a792f135311c964245625ba136e49bec
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-04 11:34:00 +0530

    add:While_Loop

---
+++ Chapter_05/WhileLoop.java
@@ -0,0 +1,21 @@
+package Chapter_05;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+//        Example 1
+        int num=1;
+        while(num <=10){
+            System.out.println(num);
+            num++;
+        }
+
+//        Example 2
+        int num1 =100;
+        while (num1<=200){
+            System.out.println(num1);
+            num1++;
+        }
+
+    }
+}
