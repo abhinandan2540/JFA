1e3b0e38578276445805a0751eab126935b3fab0
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-07 17:26:33 +0530

    method:void_type

---
+++ Chapter_07/voidType.java
@@ -0,0 +1,11 @@
+package Chapter_07;
+
+public class voidType {
+    static void Welcome(){
+        System.out.println("good morning avi");
+    }
+    public static void main(String[] args) {
+        Welcome();
+
+    }
+}
