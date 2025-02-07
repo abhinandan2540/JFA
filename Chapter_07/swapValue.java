659d70bf9b0d93ec19351b8515b7c026c5df22d2
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-07 17:27:00 +0530

    method:swap_value

--- Chapter_07/swapValue.java
+++ Chapter_07/swapValue.java
@@ -1,4 +1,22 @@
 package Chapter_07;
 
 public class swapValue {
+    static void changeInteger(int x){
+        x=98;
+    }
+
+    static void changeArray(int[] array){
+        array[0]=100;
+    }
+    public static void main(String[] args) {
+//        for integer
+    int a=45;
+    changeInteger(a);
+        System.out.println("value :"+a);
+//        for array
+        int[] marks={10,20,30};
+        changeArray(marks);
+        System.out.println("marks :"+marks[0]);
+
+    }
 }
