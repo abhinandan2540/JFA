003f3dbe76954ab8886466ebb8d062e7448dc255
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-19 16:47:05 +0530

    access_public_example

---
+++ Chapter_12/MyPackage/PublicClassExample.java
@@ -0,0 +1,4 @@
+package Chapter_12.MyPackage;
+
+public class PublicClassExample {
+}
---
+++ Chapter_12/anotherPackage/public_access.java
@@ -0,0 +1,9 @@
+package Chapter_12.anotherPackage;
+import Chapter_12.MyPackage.PublicClassExample;
+
+public class public_access {
+    public static void main(String[] args) {
+        PublicClassExample obj=new PublicClassExample();
+        obj.display();
+    }
+}
