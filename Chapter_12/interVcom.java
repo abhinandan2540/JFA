2c7400f4b28fa12f52cefd83581ea71cf90dd6ec
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-19 13:32:09 +0530

    inter & compi lang diff

---
+++ Chapter_12/interVcom.java
@@ -0,0 +1,41 @@
+package Chapter_12;
+
+public class interVcom {
+    public static void main(String[] args) {
+           /*
+           Programming languages are categorized based on how their code is translated into machine code that a computer can execute.
+            The two main categories are Interpreted Languages and Compiled Languages.
+
+            1. Interpreted Language
+            2. Compiled Language
+
+          1. Interpreted Language:
+            An interpreted language is executed line by line by an interpreter at runtime,
+            meaning the code is translated and executed simultaneously.
+
+            Character :
+           1.  Code runs without a separate compilation step.
+           2. Slower execution compared to compiled languages since translation happens during runtime.
+           3. Easier debugging because execution stops immediately at an error.
+           4. Cross-platform compatibility (as long as an interpreter is available).
+
+           Example :
+           python, javascript, php ruby
+
+         2. Compiled Language:
+           A compiled language is first converted into machine code by a compiler before execution.
+            The compiled program (binary executable) is then run separately.
+
+            Character :
+            1. Code is compiled into a separate executable file before running.
+            2. Faster execution since translation happens before runtime.
+            3. Harder debugging since errors are found after compilation.
+            4. Optimized for performance.
+
+            Example :
+            c,c++,java ( hybrid ), Go
+
+
+            */
+    }
+}
