ba2476a8a02fa6225c1c3a7e871ecf23b8646d91
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-19 17:01:55 +0530

    protected_child_class

--- Chapter_12/anotherPackage/protectec_child.java
+++ Chapter_12/anotherPackage/protectec_child.java
@@ -1,4 +1,15 @@
 package Chapter_12.anotherPackage;
+import Chapter_12.MyPackage.protected_parent;
 
-public class protectec_child {
+public class protectec_child extends protected_parent {
+    public void show(){
+        System.out.println("Acessing Protected Method :"+value);
+        display();
+    }
+
+    public static void main(String[] args) {
+        protectec_child obj=new protectec_child();
+        obj.show();
+    }
+
 }
