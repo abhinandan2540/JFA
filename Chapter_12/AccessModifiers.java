689dc07d5313731e01f0b1ab2d087160a9f56a01
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-19 16:40:02 +0530

    accessModifiers_concept

---
+++ Chapter_12/AccessModifiers.java
@@ -0,0 +1,34 @@
+package Chapter_12;
+
+// EXAMPLES ARE GIVEN FROM THE NEXT....
+
+public class AccessModifiers {
+    public static void main(String[] args) {
+//        ACCESS MODIFIERS IN JAVA PROGRAMMING LANGUAGE
+        /*
+        There are 4 types of access modifiers
+        1. public
+        2. privet
+        3. default(no)
+        4. protected
+
+        1. privet access modifiers:
+        Scope: Accessible only within the same class.
+        Usage: Used for data hiding, encapsulation, and preventing external modification.
+        Cannot be accessed from other classes, including subclasses.
+
+        2. Default access modifiers:
+        Scope: Accessible within the same package but not outside the package.
+        Usage: When you don't specify an access modifier, Java assigns the default access level.
+        Good for: Grouping related classes in the same package.
+
+        3. protected access modifiers:
+        Scope: Accessible within the same package and subclasses in different packages.
+        Usage: Used for inheritance to allow subclass access but still prevent access from unrelated classes.
+
+        4. public access modifiers:
+        Scope: Accessible everywhere (inside the same class, package, and outside package).
+        Usage: Used for methods and classes that should be globally accessible.
+         */
+    }
+}
