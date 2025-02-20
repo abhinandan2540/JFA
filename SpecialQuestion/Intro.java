7b85321ccaa0f4e3597d0e01ee6ae92756ffe51b
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-20 12:14:38 +0530

    into_to_sepcialQ

---
+++ SpecialQuestion/Intro.java
@@ -0,0 +1,21 @@
+package SpecialQuestion;
+
+public class Intro {
+    public static void main(String[] args) {
+
+        /*
+        CHALLENGE IS :
+        we need to create a package named shape and use it later for
+        1.rectangle
+        2.square
+        3.sphere
+        4.cylinder
+
+        having the property of
+        1. surface area
+        2.volume
+
+         */
+    }
+
+}
