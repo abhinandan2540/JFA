77a2a20d092d4e635ed8b9f46a549b4c3f24d259
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-20 12:53:34 +0530

    using_geoShape_Property

--- SpecialQuestion/UsingGeoShape.java
+++ SpecialQuestion/UsingGeoShape.java
@@ -1,4 +1,26 @@
 package SpecialQuestion;
 
+import SpecialQuestion.shape.GeoShapes;
+
 public class UsingGeoShape {
+    public static void main(String[] args) {
+
+//        using the square property
+        GeoShapes sqr=new GeoShapes();
+        sqr.SquareProperty(5);
+
+//        using the rectangle property
+        GeoShapes rec=new GeoShapes();
+        rec.RectangleProperty(5,4);
+
+//        using sphere property
+        GeoShapes sph=new GeoShapes();
+        sph.SphereProperty(5);
+
+//        using Cylinder property
+        GeoShapes ch=new GeoShapes();
+        ch.CylinderProperty(5,6);
+
+
+    }
 }
